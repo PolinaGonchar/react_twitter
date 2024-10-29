fb7aa9e43b3fbcf199a1c935a861ba9ee0d7c527
PolinaGonchar <gonchar.polina06@gmail.com>
2024-10-29 11:37:27 +0100

    add useReducer

--- front/src/container/post-create/index.js
+++ front/src/container/post-create/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 
 import "./index.css";
 
@@ -6,6 +6,11 @@ import FieldForm from "../../component/field-form";
 import Grid from "../../component/grid";
 
 import { Alert, Loader, LOAD_STATUS } from "../../component/load";
+import {
+  requestInitialState,
+  requestReducer,
+  REQUEST_ACTION_TYPE,
+} from "../util/request";
 
 export default function Container({
   onCreate,
@@ -13,15 +18,14 @@ export default function Container({
   button,
   id = null,
 }) {
-  const [status, setStatus] = useState(null);
-  const [message, setMessage] = useState("");
+  const [state, dispatch] = useReducer(requestReducer, requestInitialState);
 
   const handleSubmit = (value) => {
     return sendData({ value });
   };
 
   const sendData = async (dataToSend) => {
-    setStatus(LOAD_STATUS.PROGRESS);
+    dispatch({type: REQUEST_ACTION_TYPE.PROGRESS});
 
     try {
       const res = await fetch("http://localhost:4000/post-create", {
@@ -35,16 +39,14 @@ export default function Container({
       const data = await res.json();
 
       if (res.ok) {
-        setStatus(null);
+        dispatch({type: REQUEST_ACTION_TYPE.RESET});
 
         if (onCreate) onCreate();
       } else {
-        setMessage(data.message);
-        setStatus(LOAD_STATUS.ERROR);
+        dispatch({type: REQUEST_ACTION_TYPE.ERROR, message: data.message});
       }
     } catch (error) {
-      setMessage(error.message);
-      setStatus(LOAD_STATUS.ERROR);
+      dispatch({type: REQUEST_ACTION_TYPE.ERROR, message: error.message})
     }
   };
 
@@ -62,10 +64,10 @@ export default function Container({
         button={button}
         onSubmit={handleSubmit}
       />
-      {status === LOAD_STATUS.ERROR && (
-        <Alert status={status} message={message} />
+      {state.status === LOAD_STATUS.ERROR && (
+        <Alert status={state.status} message={state.message} />
       )}
-      {status === LOAD_STATUS.PROGRESS && <Loader />}
+      {state.status === LOAD_STATUS.PROGRESS && <Loader />}
     </Grid>
   );
 }
--- front/src/container/post-list/index.js
+++ front/src/container/post-list/index.js
@@ -1,4 +1,4 @@
-import { useState, Fragment, useEffect } from "react";
+import { useState, Fragment, useEffect, useReducer } from "react";
 
 import Title from "../../component/title";
 import Grid from "../../component/grid";
@@ -6,31 +6,43 @@ import Box from "../../component/box";
 
 import PostItem from "../post-item";
 import PostCreate from "../post-create";
-import { Alert, Skeleton, LOAD_STATUS } from "../../component/load";
+import { Alert, Skeleton } from "../../component/load";
 
 import { getDate } from "../util/getDate";
+import {
+  requestInitialState,
+  requestReducer,
+  REQUEST_ACTION_TYPE,
+} from "../util/request";
 
 export default function Container() {
+  const [state, dispatch] = useReducer(requestReducer, requestInitialState);
   const [status, setStatus] = useState(null);
   const [message, setMessage] = useState("");
   const [data, setData] = useState(null);
 
   const getData = async () => {
-    setStatus(LOAD_STATUS.PROGRESS);
+    dispatch({type: REQUEST_ACTION_TYPE.PROGRESS});
     try {
       const res = await fetch("http://localhost:4000/post-list");
 
       const data = await res.json();
       if (res.ok) {
-        setData(convertData(data));
-        setStatus(LOAD_STATUS.SUCCESS);
+        dispatch({
+          type : REQUEST_ACTION_TYPE.SUCCESS,
+          payload: convertData(data),
+        });
       } else {
-        setMessage(data.message);
-        setStatus(LOAD_STATUS.ERROR);
+        dispatch({
+          type: REQUEST_ACTION_TYPE.ERROR,
+          payload : data.message,
+        })
       }
     } catch (error) {
-      setMessage(error.message);
-      setStatus(LOAD_STATUS.ERROR);
+      dispatch({
+        type: REQUEST_ACTION_TYPE.ERROR,
+        payload : error.message,
+      })
     }
   };
 
@@ -47,16 +59,7 @@ export default function Container() {
 
   useEffect(() => {
     getData();
-
-    const intervalId = setInterval(() => getData(), 5000)
-    return () => {
-      clearInterval(intervalId);
-    };
   }, []);
-
-  if (status === null) {
-    getData();
-  }
 
   return (
     <Grid>
@@ -71,7 +74,7 @@ export default function Container() {
         </Grid>
       </Box>
 
-      {status === LOAD_STATUS.PROGRESS && (
+      {state.status === REQUEST_ACTION_TYPE.PROGRESS && (
         <Fragment>
           <Box>
             <Skeleton />
@@ -82,16 +85,16 @@ export default function Container() {
         </Fragment>
       )}
 
-      {status === LOAD_STATUS.ERROR && (
-        <Alert status={status} message={message} />
+      {state.status === REQUEST_ACTION_TYPE.ERROR && (
+        <Alert status={state.status} message={state.message} />
       )}
 
-      {status === LOAD_STATUS.SUCCESS && (
+      {state.status === REQUEST_ACTION_TYPE.SUCCESS && (
         <Fragment>
-          {data.isEmpty ? (
+          {state.data.isEmpty ? (
             <Alert message="Post list is empty" />
           ) : (
-            data.list.map((item) => (
+            state.data.list.map((item) => (
               <Fragment key={item.id}>
                 <PostItem {...item} />
               </Fragment>
